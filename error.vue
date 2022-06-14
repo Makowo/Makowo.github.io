13097562ace4f3e820fe77eb716ca125a13046db
Mako <masterjon902@gmail.com>
2022-06-14 15:23:11 -0500

    figuring this out still

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex justify-center flex-col items-center h-screen">
+    <h1>{{"Error: " + error.statusCode}}</h1>
+    <h1>{{error.statusMessage}}</h1>
+    <button class="btn" @click="handleError">Return to Homepage</button>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  error: Object
+})
+
+const handleError = () => clearError({ redirect: '/' })
+</script>
